7e7cec22fdb7d74cbe3f5ca8c3a61ce4dcd39cc8
Hobart2967 <hobart@codewyre.net>
2023-03-25 02:00:06 +0100

    feat(datanase): :card_file_box: Add database container and initial script

---
+++ .config/db/0.schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE taskify;
+USE taskify;
+CREATE TABLE todos (
+  id varchar(255),
+  `subject` varchar(1000),
+  `author` varchar(255)
+);
---
+++ .config/db/1.data.sql
@@ -0,0 +1,10 @@
+USE taskify;
+
+INSERT INTO todos (
+  id,
+  `subject`,
+  `author`
+) VALUES (
+  'd93e5537-f12e-456b-8e65-d70a7117edf8',
+  'Clean my room',
+  'ec1de64a-45f6-4d70-9f66-4efd26650f3c');
