0a8e563ab7a2a5fa15fbf8fae22602cf2008de0d
Hobart2967 <hobart@codewyre.net>
2023-03-25 01:10:25 +0100

    feat(todos): :lipstick: Add more structure and design to the todos-list

--- web/src/components/todo-list.component.tsx
+++ web/src/components/todo-list.component.tsx
@@ -9,11 +9,24 @@ export function TodoListComponent() {
     () => todoService.getMyTodos(),
     { initialValue: [] });
 
-  return (<>
-    <For each={todos()}>
-      {todo => (
-        <div>{todo.Title}</div>
-      )}
-    </For>
-  </>);
+  return (<div class="todo-list">
+    <div>
+      <h1>Open Todos:</h1>
+    </div>
+    <div class="todo-list-items">
+      <For each={todos()}>
+        {todo => (
+          <div class="todo-list-items__entry">
+            <div>
+              <div class="checkbox">
+                <input type="checkbox" />
+              </div>
+            </div>
+            <div>{todo.title}</div>
+            <div>{todo.author}</div>
+          </div>
+        )}
+      </For>
+    </div>
+  </div>);
 }
