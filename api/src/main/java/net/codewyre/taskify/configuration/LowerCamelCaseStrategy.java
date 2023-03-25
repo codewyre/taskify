15bf587edee6d624df1f7ff44245ffbeaae10b57
Hobart2967 <hobart@codewyre.net>
2023-03-25 01:09:43 +0100

    fix(todos): :bug: Fix getTodos and other endpoint not returning "lowerCase"-first property names

---
+++ api/src/main/java/net/codewyre/taskify/configuration/LowerCamelCaseStrategy.java
@@ -0,0 +1,17 @@
+package net.codewyre.taskify.configuration;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.NamingBase;
+
+public class LowerCamelCaseStrategy extends NamingBase {
+  private static final long serialVersionUID = 2L;
+
+  /**
+   * @since 2.14
+   */
+  public final static LowerCamelCaseStrategy INSTANCE = new LowerCamelCaseStrategy();
+
+  @Override
+  public String translate(String input) {
+      return input.substring(0, 1).toLowerCase() + input.substring(1);
+  }
+}
