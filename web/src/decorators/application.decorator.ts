b097d4f757428b133928d5ea118af4e89d40fd0e
Hobart2967 <hobart@codewyre.net>
2023-03-26 15:09:19 +0200

    fix: :rotating_light: Fix linting warnings in decorator

--- web/src/decorators/application.decorator.ts
+++ web/src/decorators/application.decorator.ts
@@ -1,6 +1,7 @@
-export function application(): Function {
-  return (constructor: Function) => {
+export function application(): ClassDecorator {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return  <TFunction extends Function>(constructor: TFunction) => {
     setTimeout(() => (constructor as any).main());
     return constructor;
-  }
+  };
 }
