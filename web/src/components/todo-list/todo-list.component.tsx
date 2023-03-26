a8880c6b93f6fef3a56cdaefffc337e6e38de1df
Hobart2967 <hobart@codewyre.net>
2023-03-26 18:26:16 +0200

    feat(todos): :sparkles: Add empty list view

--- web/src/components/todo-list/todo-list.component.tsx
+++ web/src/components/todo-list/todo-list.component.tsx
@@ -1,4 +1,4 @@
-import { createResource, For, JSX } from 'solid-js';
+import { createResource, For, JSX, Show } from 'solid-js';
 import { useDependency } from '../../contexts/dependency-injection.context';
 import { TodoService } from '../../services/todo.service';
 
@@ -14,19 +14,26 @@ export function TodoListComponent(): JSX.Element {
       <h1>Open Todos:</h1>
     </div>
     <div class="todo-list-items">
-      <For each={todos()}>
-        {todo => (
-          <div class="todo-list-items__entry">
-            <div>
-              <div class="checkbox">
-                <input type="checkbox" />
+      <Show when={!todos().length}>
+        <div class="done">
+          Congrats! You have done everything that we know of. If you miss something, add it below this list.
+        </div>
+      </Show>
+      <Show when={todos().length}>
+        <For each={todos()}>
+          {todo => (
+            <div class="todo-list-items__entry">
+              <div>
+                <div class="checkbox">
+                  <input type="checkbox" />
+                </div>
               </div>
+              <div>{todo.title}</div>
+              <div>{todo.author}</div>
             </div>
-            <div>{todo.title}</div>
-            <div>{todo.author}</div>
-          </div>
-        )}
-      </For>
+          )}
+        </For>
+      </Show>
     </div>
   </div>);
 }
